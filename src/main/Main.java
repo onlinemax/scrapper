c5b7d345564f9f2c8a4e8886b005859ce601201d
onlinemax <91437969+onlinemax@users.noreply.github.com>
2024-05-21 23:48:50 +0000

    Added the main class

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+import request.Request;
+public class Main{
+    public static void main(String[] args) {
+        Request req = new Request("allo");
+        System.out.println("Hello World");
+    }
+}
