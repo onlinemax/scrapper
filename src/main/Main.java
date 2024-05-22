2ecd21df9099c6f919cf3cab0ebfb045fbb0ccb6
onlinemax <91437969+onlinemax@users.noreply.github.com>
2024-05-22 00:38:56 +0000

    started to request our first file for testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,18 @@
 package main;
+import java.io.File;
+import java.io.IOException;
 import request.Request;
 public class Main{
     public static void main(String[] args) {
-        Request req = new Request("allo");
-        System.out.println("Hello World");
+        Request req = new Request("https://usito.usherbrooke.ca/index/articles#a");
+        req.establishConnection();
+        File f = new File(System.getProperty("user.dir") + "/test/output.txt");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        req.writeBodyToFile(f);    
     }
 }
--- src/request/Request.java
+++ src/request/Request.java
@@ -1,6 +1,66 @@
 package request;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 public class Request{
-    public Request(String url){
+    private final String address;
+    private String body;
+    public Request(String address){
+        this.address = address;
+    } 
+    public String getBody(){
+        return body;
+    }
+    public void writeBodyToFile(File file){
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(body);            
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void establishConnection(){
+        URL url = null;
+        this.body = "";
+        try{
+            url = new URI(address).toURL();       
+            
+        }catch(URISyntaxException e){
+            System.err.println(e.getMessage());
+        }
+        catch(MalformedURLException e){
+            System.err.println(e.getMessage());
+        }
+        try{
+            HttpURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.connect();
+            System.out.println("Connection gives response code: " + connection.getResponseCode());
+            this.body = getResponseString(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+            connection.disconnect();
+        }catch(IOException e){
+            System.err.println("Can't connect");
+        }
+
+
 
     }
+    private String getResponseString(BufferedReader reader) throws IOException{
+        StringBuilder builder = new StringBuilder("");
+        while (reader.ready()){ 
+            builder.append(reader.readLine());
+        } 
+        return builder.toString();
+    }
+    
+   
 }
